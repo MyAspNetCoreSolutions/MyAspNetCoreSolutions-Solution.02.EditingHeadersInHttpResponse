e6738b6247b99763e24d08ca415ce96e76f71e1e
Mhammad lotfi <lotfi.engin@gmail.com>
2018-02-13 09:44:59 +0330

    Add project files.

---
+++ Tamrin/LotfiResponseEditingMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace Tamrin
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class LotfiResponseEditingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IConfigurationRoot _configurationRoot;
+
+        public LotfiResponseEditingMiddleware(RequestDelegate next, IConfigurationRoot confiqurationRoot)
+        {
+            _next = next;
+            _configurationRoot= confiqurationRoot;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            await _next(httpContext);
+
+            var headerSection = _configurationRoot.GetSection("MySection");
+            string s = "";
+            foreach (var header in headerSection.GetChildren())
+            {
+               
+                    httpContext.Response.Headers.Add(header.Key, header.Value);
+                s += "   " + header.Key + " - > " + header.Value;
+            }
+            await httpContext.Response.WriteAsync(s);
+
+
+        }
+    }  
+}
---
+++ Tamrin/Startup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Tamrin
+{
+    public class Startup
+    {
+        IConfigurationRoot configuration;
+
+        public Startup(IHostingEnvironment env)
+        {
+            var Builder = new ConfigurationBuilder().SetBasePath(env.ContentRootPath).AddJsonFile("appsettings.json");
+            configuration = Builder.Build();
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(typeof(IConfigurationRoot), configuration);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+
+            app.UseMiddleware<LotfiResponseEditingMiddleware>();
+            
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // وقتی از خط زیر استفاده میکنم این خطا رو می گیرم
+            // An unhandled exception was thrown by the application.
+            // System.InvalidOperationException: Headers are read - only, response has already started.
+
+            app.Run(async (context) =>
+            {
+                //await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
